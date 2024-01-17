40e2f9edca7c8f13a6574b9ad36b886141197c5f
srdt-ai <61540090+srdt-ai@users.noreply.github.com>
2024-01-17 17:46:28 +0100

    Add files via upload

---
+++ segmentation_texte.py
@@ -0,0 +1,28 @@
+import re
+
+def tokenize(text):
+    pattern = re.compile(r'[A-Za-z]+[\w^\']*|[\w^\']*[A-Za-z]+[\w^\']*')
+    return pattern.findall(text.lower())
+
+tokens = tokenize(text)
+
+def mapping(tokens):
+    word_to_id = {}
+    id_to_word = {}
+    
+    for i, token in enumerate(set(tokens)):
+        word_to_id[token] = i
+        id_to_word[i] = token
+    
+    return word_to_id, id_to_word
+
+word_to_id, id_to_word = mapping(tokens)
+word_to_id
+
+
+
+
+
+
+
+
